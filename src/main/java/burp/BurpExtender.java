a1b2a4da1d8215e9a07331eae7f4b40627f045d2
Hannah-PortSwigger <58562826+Hannah-PortSwigger@users.noreply.github.com>
2022-02-14 13:23:03 +0000

    Add try/catch block

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,5 +1,6 @@
 package burp;
 
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.File;
 
@@ -9,10 +10,11 @@ import java.time.LocalDateTime;
 public class BurpExtender implements IBurpExtender, IExtensionStateListener
 {
     private String folderPath = "/var/log/BurpSuiteEnterpriseEdition/";     // CHANGE ME!
+//    private String folderPath = "/Users/hannah.law/Documents/extensiontest/";     // CHANGE ME!
     private String fileName;
 
     private IBurpExtenderCallbacks callbacks;
-    private PrintWriter stdout;
+    private PrintWriter stdout, stderr;
     private static String NAME = "Generate XML Report extension";
     private LocalDateTime extensionLoadedTime;
 
@@ -22,26 +24,33 @@ public class BurpExtender implements IBurpExtender, IExtensionStateListener
         this.callbacks = callbacks;
 
         stdout = new PrintWriter(callbacks.getStdout(), true);
+        stderr = new PrintWriter(callbacks.getStderr(), true);
 
         callbacks.registerExtensionStateListener(this);
         callbacks.setExtensionName(NAME);
 
         extensionLoadedTime = LocalDateTime.now();
 
-        stdout.println( NAME + " loaded");
+        stdout.println( NAME + " - Loaded");
     }
 
     @Override
     public void extensionUnloaded()
     {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss");
-
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm");
         fileName = "xml-report-" + dtf.format(extensionLoadedTime) + ".xml";
-
         File file = new File(folderPath + fileName);
 
-        callbacks.generateScanReport("XML", callbacks.getScanIssues(null), file);
+        IScanIssue[] scanIssues = callbacks.getScanIssues(null);
 
-        stdout.println( NAME + " - XML scan report saved at: " + file);
+        try
+        {
+            callbacks.generateScanReport("XML", scanIssues, file);
+            stdout.println( NAME + " - XML scan report saved at: " + file);
+        }
+        catch (RuntimeException e)
+        {
+            stderr.println(NAME + " - XML report failed:\n" + e);
+        }
     }
 }
