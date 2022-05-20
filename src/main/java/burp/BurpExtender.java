4338725cd7ad515b05c272a14087cf680bcaf596
nullspace <39434250+nullspace-dev@users.noreply.github.com>
2022-05-20 13:44:12 -0400

    Update BurpExtender.java
    Added command execution of a shell script to handle a system level command to send an email of those two attachments. 
    which just needs this line to function as well as mail-utils to be installed. This allows for a secure email to be sent using the systems email tooling without exposing credentials in the extension code.
    
    echo "Vuln scan report from burpSuite" | mailx  -s 'BurpSuite Report' -a $1 -a $2 -r "fromEmailaddress@domain.com" toEmailAddress@domain.com

--- src/main/java/burp/BurpExtender.java
+++ src/main/java/burp/BurpExtender.java
@@ -1,26 +1,45 @@
 package burp;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
 import java.io.File;
 
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.net.URL;
+import java.util.Properties;
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
-public class BurpExtender implements IBurpExtender, IExtensionStateListener
-{
-    private String folderPath = "/var/log/BurpSuiteEnterpriseEdition/";     // CHANGE ME!
-    private String fileName;
-
+public class BurpExtender implements IBurpExtender, IExtensionStateListener, IHttpListener{
+    private String folderPath = "/var/log/BurpSuiteEnterpriseEdition/scanResults";     // CHANGE ME!
+    private String xmlFileName;
+    private String htmlFileName;
+    
     private IBurpExtenderCallbacks callbacks;
+    private IExtensionHelpers helpers;
+
     private PrintWriter stdout, stderr;
     private static String NAME = "Generate XML Report extension";
     private LocalDateTime extensionLoadedTime;
-
+    private final List<LogEntry> log = new ArrayList<LogEntry>();
+    private String siteName;
+    
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
     {
         this.callbacks = callbacks;
+        // obtain an extension helpers object
+        helpers = callbacks.getHelpers();
 
         stdout = new PrintWriter(callbacks.getStdout(), true);
         stderr = new PrintWriter(callbacks.getStderr(), true);
@@ -28,6 +47,7 @@ public class BurpExtender implements IBurpExtender, IExtensionStateListener
         callbacks.registerExtensionStateListener(this);
         callbacks.setExtensionName(NAME);
 
+        
         extensionLoadedTime = LocalDateTime.now();
 
         stdout.println( NAME + " - Loaded");
@@ -37,19 +57,135 @@ public class BurpExtender implements IBurpExtender, IExtensionStateListener
     public void extensionUnloaded()
     {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm");
-        fileName = "xml-report-" + dtf.format(extensionLoadedTime) + ".xml";
-        File file = new File(folderPath + fileName);
+        
+        xmlFileName = siteName + "xml-report-" + dtf.format(extensionLoadedTime) + ".xml";
+        File xmlFile = new File(folderPath +  xmlFileName);
+        
+        htmlFileName = siteName + "xml-report-" + dtf.format(extensionLoadedTime) + ".xml";
+        File htmlFile = new File(folderPath +  htmlFileName);
 
         IScanIssue[] scanIssues = callbacks.getScanIssues(null);
-
+        
         try
         {
-            callbacks.generateScanReport("XML", scanIssues, file);
-            stdout.println( NAME + " - XML scan report saved at: " + file);
+            callbacks.generateScanReport("XML", scanIssues, xmlFile);
+            stdout.println( NAME + " - XML scan report saved at: " + xmlFile);
+                      
         }
         catch (RuntimeException e)
         {
             stderr.println(NAME + " - XML report failed:\n" + e);
         }
+        
+        try {
+            callbacks.generateScanReport("HTML", scanIssues, htmlFile);
+            stdout.println( NAME + " - HTML scan report saved at: " + htmlFile);
+        } catch (RuntimeException e)
+        {
+            stderr.println(NAME + " - HTML report failed:\n" + e);
+        }
+        
+        
+        String command = "sh /opt/burpsuite_enterprise/sendEmail.sh "+ xmlFullPath + " "+ htmlFullPath;
+        
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+         
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+         
+            reader.close();
+         
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo)
+    {
+        // only process responses
+        if (!messageIsRequest)
+        {
+            // create a new log entry with the message details
+            synchronized(log)
+            {
+                
+                log.add(new LogEntry(toolFlag, callbacks.saveBuffersToTempFiles(messageInfo), 
+                        helpers.analyzeRequest(messageInfo).getUrl()));
+                
+            }
+            if(siteName.length() != 0)
+            	siteName = helpers.analyzeRequest(messageInfo).getUrl().getHost();
+            
+        }
+    }
+    
+    private static class LogEntry
+    {
+        final int tool;
+        final IHttpRequestResponsePersisted requestResponse;
+        final URL url;
+
+        LogEntry(int tool, IHttpRequestResponsePersisted requestResponse, URL url)
+        {
+            this.tool = tool;
+            this.requestResponse = requestResponse;
+            this.url = url;
+        }
+    }
+    
+    private static void sendMail(File file) {
+        final String username = "your.mail.id@gmail.com";
+        final String password = "your.password";
+        
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.starttls.enable", true);
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("from.mail.id@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("to.mail.id@gmail.com"));
+            message.setSubject("Testing Subject");
+            message.setText("Burp scan ");
+
+            MimeBodyPart messageBodyPart = new MimeBodyPart();
+
+            Multipart multipart = new MimeMultipart();
+            
+            //String file = "path of file to be attached";
+            String fileName = "attachmentName";
+            DataSource source = new FileDataSource(file);
+            messageBodyPart.setDataHandler(new DataHandler(source));
+            messageBodyPart.setFileName(fileName);
+            multipart.addBodyPart(messageBodyPart);
+
+            message.setContent(multipart);
+
+            System.out.println("Sending");
+
+            Transport.send(message);
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    
     }
 }
